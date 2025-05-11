a1ec5ff534766d535bf4718ebc40f7cd6ae2bfe3
ChamzalIsal <93560711+AkanTahu@users.noreply.github.com>
2025-05-11 10:40:39 +0700

    prepare win deploy

--- .docker/phpmyadmin/wait-for-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "⌛ Waiting for DB to be ready..."
-until nc -z db 3306; do
-  sleep 2
-done
-echo "✅ DB is ready, launching phpMyAdmin"
-exec "$@"
--- .docker/rekachain-web/wait-for-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-until mysqladmin ping -h db --silent; do
-    echo "⚡ Waiting for db to be ready..."
-    sleep 2
-done
-echo "✅ Database is ready!"
-exec "$@"
